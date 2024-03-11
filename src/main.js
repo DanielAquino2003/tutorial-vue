da7dbe71245eda41db6b474fdedacf68de13662b
daniel <aquinosantiagodani@gmail.com>
2024-03-11 20:32:32 +0100

    adding tuto vue

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const myapp = createApp(App)
+const pinia = createPinia()
+
+myapp.use(pinia)
+myapp.mount('#app')
+
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
